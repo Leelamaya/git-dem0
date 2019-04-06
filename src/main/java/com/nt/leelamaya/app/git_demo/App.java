a582208a1323f77e148b4c6c2f606986449ef52b
Leelamaya <leelamayajena1@gmail.com>
2019-04-06 11:28:34 +0530

    First commit

---
+++ src/main/java/com/nt/leelamaya/app/git_demo/App.java
@@ -0,0 +1,13 @@
+package com.nt.leelamaya.app.git_demo;
+
+
+public class App 
+{
+	public void addData(String data) {
+		System.out.println("data" + data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
